c86816ac4963d2f1bf6d50bb82bd44a919e971ff
Mikhail Titov <mikhail.a.titov@gmail.com>
2017-09-13 15:50:20 +0300

    new: policies - core blocks' behaviour definition (its initial implementation includes possibility to define queue limit per stream)

---
+++ qss/policies.py
@@ -0,0 +1,22 @@
+#
+# Copyright European Organization for Nuclear Research (CERN)
+#           National Research Centre "Kurchatov Institute"
+#           Rutgers University
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Author(s):
+# - Mikhail Titov, <mikhail.titov@cern.ch>, 2017
+#
+
+from .constants import StreamName
+
+QUEUE_POLICY = {
+    'limit': {
+        StreamName.Main: 4,
+    }
+}
