875d070d921654413eaf4e109a4148b4297a9f93
Mikhail Titov <mikhail.a.titov@gmail.com>
2017-09-13 12:00:54 +0300

    separate module for constants

---
+++ qss/constants.py
@@ -0,0 +1,29 @@
+#
+# Copyright European Organization for Nuclear Research (CERN)
+#           National Research Centre "Kurchatov Institute"
+#           Rutgers University
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Author(s):
+# - Mikhail Titov, <mikhail.titov@cern.ch>, 2017
+#
+
+from .utils import EnumTypes
+
+
+ActionCode = EnumTypes(
+    ('Arrival', 'a'),
+    ('Submission', 's'),
+    ('Completion', 'c'),
+)
+
+ServiceState = EnumTypes(
+    ('Arrival', 0),
+    ('Completion', 1),
+    ('Stop', 2)
+)
