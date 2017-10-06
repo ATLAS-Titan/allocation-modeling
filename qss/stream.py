d3980bd4f3298e58afca28f533fe0fa38e2de72c
Mikhail Titov <mikhail.a.titov@gmail.com>
2017-10-06 15:17:16 +0300

    added possibility to define jobs' parameters (arrival_timestamp, execution_time, num_nodes) by input file

--- qss/stream.py
+++ qss/stream.py
@@ -14,6 +14,7 @@
 #
 
 import math
+import os
 import random
 
 from .core.job import Job
@@ -57,12 +58,59 @@ def stream_generator(arrival_rate, execution_rate, num_nodes=None,
     next_arrival_timestamp = get_random(arrival_rate)
     while num_jobs or (time_limit and next_arrival_timestamp < time_limit):
 
-        yield Job(execution_time=get_random(execution_rate),
-                  arrival_timestamp=next_arrival_timestamp,
-                  source_label=source_label,
-                  num_nodes=num_nodes_per_job)
+        yield Job(arrival_timestamp=next_arrival_timestamp,
+                  execution_time=get_random(execution_rate),
+                  num_nodes=num_nodes_per_job,
+                  source_label=source_label)
 
         next_arrival_timestamp += get_random(arrival_rate)
 
         if num_jobs:
             num_jobs -= 1
+
+
+def stream_generator_by_file(file_name, source_label=None, time_limit=None):
+    """
+    Yield jobs with specified arrival and service times.
+
+    @param file_name: File name with input data.
+    @type file_name: str
+    @param source_label: Name of the job's source.
+    @type source_label: str/None
+    @param time_limit: The maximum timestamp (until generation is done).
+    @type time_limit: float/None
+    @return: Job object.
+    @rtype: qss.core.job.Job
+    """
+    if not file_name or not os.path.exists(file_name):
+        raise Exception('Input data is not defined (wrong file path).')
+
+    source_label = source_label or SOURCE_LABEL_DEFAULT
+
+    latest_arrival_timestamp = arrival_timestamp = 0.
+    while not time_limit or (latest_arrival_timestamp < time_limit):
+
+        with open(file_name, 'r') as f:
+            for line in f:
+                job_params = line.split(',')
+
+                try:
+                    arrival_timestamp = (
+                        float(job_params[0]) + latest_arrival_timestamp)
+                    execution_time = float(job_params[1])
+                    num_nodes = int(float(job_params[2]))
+                except ValueError:
+                    continue
+
+                if time_limit and arrival_timestamp > time_limit:
+                    break
+
+                yield Job(arrival_timestamp=arrival_timestamp,
+                          execution_time=execution_time,
+                          num_nodes=num_nodes,
+                          source_label=source_label)
+
+        if not time_limit or not arrival_timestamp:
+            break
+
+        latest_arrival_timestamp = arrival_timestamp
