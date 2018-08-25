af922f4657068b1dc18850457f11d14145ee8682
Mikhail Titov <mikhail.a.titov@gmail.com>
2018-08-26 00:59:47 +0300

    new stream with Titan requirements for randomly generated parameters

--- qss/stream.py
+++ qss/stream.py
@@ -17,13 +17,11 @@ import math
 import os
 import random
 
+from .constants import StreamName
 from .core.job import Job
 
-from .constants import StreamName
-
-
-SOURCE_LABEL_DEFAULT = StreamName.Default
 NUM_NODES_DEFAULT = 1
+SOURCE_LABEL_DEFAULT = StreamName.Default
 
 
 def stream_generator(arrival_rate, execution_rate, num_nodes=None,
@@ -52,11 +50,11 @@ def stream_generator(arrival_rate, execution_rate, num_nodes=None,
     if not num_jobs and not time_limit:
         raise Exception('Limits are not set.')
 
-    num_nodes_per_job = num_nodes or NUM_NODES_DEFAULT
-    source_label = source_label or SOURCE_LABEL_DEFAULT
-
     def get_random(rate):
         return (-1. / rate) * math.log(1. - random.random())
+
+    num_nodes_per_job = num_nodes or NUM_NODES_DEFAULT
+    source_label = source_label or SOURCE_LABEL_DEFAULT
 
     next_arrival_timestamp = first_arrival_timestamp or get_random(arrival_rate)
     while num_jobs or (time_limit and next_arrival_timestamp < time_limit):
@@ -64,6 +62,60 @@ def stream_generator(arrival_rate, execution_rate, num_nodes=None,
         yield Job(arrival_timestamp=next_arrival_timestamp,
                   execution_time=get_random(execution_rate),
                   num_nodes=num_nodes_per_job,
+                  source_label=source_label)
+
+        next_arrival_timestamp += get_random(arrival_rate)
+
+        if num_jobs:
+            num_jobs -= 1
+
+
+def stream_generator_titan(arrival_rate, source_label=None,
+                           first_arrival_timestamp=None,
+                           num_jobs=None, time_limit=None):
+    """
+    Yield jobs with randomly generated parameters (Titan requirements):
+       arrival timestamp, wall time, execution time, number of nodes.
+
+    @param arrival_rate: Arrival rate for jobs.
+    @type arrival_rate: float
+    @param source_label: Name of the job's source.
+    @type source_label: str/None
+    @param first_arrival_timestamp: Initial (first) arrival timestamp.
+    @type first_arrival_timestamp: float
+    @param num_jobs: Number of generated jobs (None -> infinite jobs).
+    @type: num_jobs: int/None
+    @param time_limit: The maximum timestamp (until generation is done).
+    @type time_limit: float/None
+    @return: Job object.
+    @rtype: qss.core.job.Job
+    """
+    if not num_jobs and not time_limit:
+        raise Exception('Limits are not set.')
+
+    from .policy import TITAN_REQUESTED_PROCESSOR_COUNT
+
+    def get_random(rate):
+        return (-1. / rate) * math.log(1. - random.random())
+
+    def get_random_range(start, end):
+        if start < end:
+            return (-1. * random.uniform(start, end) *
+                    math.log(1. - random.random()))
+
+    source_label = source_label or SOURCE_LABEL_DEFAULT
+    groups = TITAN_REQUESTED_PROCESSOR_COUNT.keys()
+
+    next_arrival_timestamp = first_arrival_timestamp or get_random(arrival_rate)
+    while num_jobs or (time_limit and next_arrival_timestamp < time_limit):
+
+        val = TITAN_REQUESTED_PROCESSOR_COUNT[random.randint(groups)]
+        wall_time = (int(random.uniform(1., val['max_walltime'] / 60.)) * 60.)
+
+        yield Job(arrival_timestamp=next_arrival_timestamp,
+                  wall_time=wall_time,
+                  execution_time=get_random_range(1., wall_time),
+                  num_nodes=int(random.uniform(*val['nodes'])),
                   source_label=source_label)
 
         next_arrival_timestamp += get_random(arrival_rate)
